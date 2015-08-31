54ea2237898e015bb858882513bc12800be16d5a
mattphillips <mattphillips1016@gmail.com>
2015-08-31 13:09:41 +0100

    Added basic interface definition of a stack

---
+++ src/main/java/io/mattphillips/data/stack/MyStack.java
@@ -0,0 +1,27 @@
+package io.mattphillips.data.stack;
+
+import io.mattphillips.data.Node;
+
+public class MyStack<T> implements Stack<T> {
+
+	private Node<T> stack;
+
+	@Override
+	public void pop() {
+	}
+
+	@Override
+	public void push(T item) {
+	}
+
+	@Override
+	public T peek() {
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+	
+} 
---
+++ src/main/java/io/mattphillips/data/stack/Stack.java
@@ -0,0 +1,16 @@
+package io.mattphillips.data.stack;
+
+public interface Stack<T> {
+
+	// Remove the top item from the stack
+	public void pop();
+
+	// Add item to the top of the stack
+	public void push(T item);
+
+	// Return the top of the stack
+	public T peek();
+
+	// Return true if and only if the stack is empty
+	public boolean isEmpty();
+}
